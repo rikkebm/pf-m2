048246cf1a71b0654c796a2459a5c171e2560898
rikkebm <60385097+rikkebm@users.noreply.github.com>
2020-03-15 13:55:04 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+//  question 1
+
+//  var petArray = ["cat","cow","dog"];
+
+//  question 2
+
+//  console.log (petArray[1]);
+
+//  question 3
+
+//  petArray.push("Sheep");
+
+//  question 4
+
+//  console.log(petArray.length);
+
+//  question 5
+
+//  var catObject ={
+//  name: "Patsy",
+//  color: "brown",
+//  age: 8
+//  }
+
+//  question 6
+
+//  console.log(catObject.age);
+
+//  question 7
+
+//  var catArray = ["Patsy","brown",8];
+
+//  question 8
+
+//  for (var i=0; i <catArray.length; i++){
+//  console.log(catArray[i]);
+
+//  question 9
+
+//  function logToConsole(theArgument) {
+//  console.log(theArgument);
+//  }
+
+// question 10
+
+//logToConsole("donkey");
